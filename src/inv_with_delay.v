da120b5a1c56fe4010279ca730c0a3a433adc3b4
Mike Bell <mike@mercuna.com>
2023-03-15 20:44:14 +0000

    Gate level simulation of the ring oscillator

---
+++ src/inv_with_delay.v
@@ -0,0 +1,28 @@
+// This is a copy of sky130_fd_sc_hd__inv but with a #20 delay on the not for simulation purposes.
+module inv_with_delay(
+    Y   ,
+    A   ,
+    VPWR,
+    VGND,
+    VPB ,
+    VNB
+);
+
+    // Module ports
+    output Y   ;
+    input  A   ;
+    input  VPWR;
+    input  VGND;
+    input  VPB ;
+    input  VNB ;
+
+    // Local signals
+    wire not0_out_Y       ;
+    wire pwrgood_pp0_out_Y;
+
+    //                                 Name         Output             Other arguments
+    not #20                            not0        (not0_out_Y       , A                     );
+    sky130_fd_sc_hd__udp_pwrgood_pp$PG pwrgood_pp0 (pwrgood_pp0_out_Y, not0_out_Y, VPWR, VGND);
+    buf                                buf0        (Y                , pwrgood_pp0_out_Y     );
+
+endmodule
